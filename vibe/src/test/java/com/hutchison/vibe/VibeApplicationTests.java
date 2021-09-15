ab45ca035a3bcbe1f399f439eccf5349501f7586
Sean Hutchison <xskhutchx@gmail.com>
2021-09-14 19:30:38 -0600

    f

---
+++ vibe/src/test/java/com/hutchison/vibe/VibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutchison.vibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
